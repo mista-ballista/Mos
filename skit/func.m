6abda7bf7706dec0b449e8fcbf2b5341c6f8ac6d
Alexander Johansson <alex.json88@gmail.com>
2012-01-31 16:57:54 +0100

    Signed-off-by: Alexander Johansson <alex.json88@gmail.com>

--- skit/func.m
+++ skit/func.m
@@ -19,9 +19,9 @@ L=0.95;
 y=0.25;
 theta0=acosd((L-y)/l);
 x0=l*sind(theta0);
-theta=(0.731196*180)/pi;
+theta=(0.731196*180)/pi
 
-(2.90035*180)/pi;
+(2.90035*180)/pi
 
 k=(l*L*sind(theta0)*kappa)/2*theta0
 
@@ -33,7 +33,7 @@ v0=(l*cosd(alpha-theta))/cos(alpha)
 % Test = solve(0.75*sind(X)+0.95*cosd(arcsind((0.25+0.75*cos(X))/0.95))-1)
 % Test = solve(0.75*sind(X)+0.95*cosd(arcsind((0.25+0.75*cos(X))/0.95))-x)
 % 0.75*sin(X)+0.95cos(arcsin((0.25+0.75*cos(X))/0.95))=1
-X = solve('0.75*sind(X)+0.95*cosd(arcsind((0.25+0.75*cos(X))/0.95))-1=0')
+% X = solve('0.75*sind(X)+0.95*cosd(arcsind((0.25+0.75*cos(X))/0.95))-1=0')
 
 % x är draglängd
 x= l*sind(theta)+L*cosd(alpha);
