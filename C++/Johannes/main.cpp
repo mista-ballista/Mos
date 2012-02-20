82f096cb67b5711eb7a9b264088d513ee4231d42
Johannes Dahlgren <lucifer54321@gmail.com>
2012-02-20 14:04:07 +0100

    Heightmap med textur!

--- C++/Johannes/main.cpp
+++ C++/Johannes/main.cpp
@@ -19,11 +19,17 @@
 /* File for "Terrain" lesson of the OpenGL tutorial on
  * www.videotutorialsrock.com
  */
-
+#define WIN32_LEAN_AND_MEAN
 
 
 #include <iostream>
 #include <stdlib.h>
+
+
+#include <windows.h>
+#include <gl/gl.h>
+#include <gl/glu.h>
+#include <gl/glaux.h>
 
 #ifdef __APPLE__
 #include <OpenGL/OpenGL.h>
@@ -34,6 +40,10 @@
 
 #include "imageloader.h"
 #include "vec3f.h"
+//#include "vkgllib.h"
+
+#define BITMAP_ID 0x4D42
+
 
 using namespace std;
 
@@ -187,6 +197,7 @@ class Terrain {
 		}
 };
 
+
 //Loads a terrain from a heightmap.  The heights of the terrain range from
 //-height / 2 to height / 2.
 Terrain* loadTerrain(const char* filename, float height) {
@@ -206,8 +217,76 @@ Terrain* loadTerrain(const char* filename, float height) {
 	return t;
 }
 
+unsigned char *LoadBitmapFile(char *filename, BITMAPINFOHEADER *bitmapInfoHeader)
+{
+	FILE *filePtr;							      // the file pointer
+	BITMAPFILEHEADER	bitmapFileHeader;		// bitmap file header
+	unsigned char		*bitmapImage;			// bitmap image data
+	int					imageIdx = 0;		   // image index counter
+	unsigned char		tempRGB;				   // swap variable
+
+	// open filename in "read binary" mode
+	filePtr = fopen(filename, "rb");
+	if (filePtr == NULL)
+		return NULL;
+
+	// read the bitmap file header
+	fread(&bitmapFileHeader, sizeof(BITMAPFILEHEADER), 1, filePtr);
+	
+	// verify that this is a bitmap by checking for the universal bitmap id
+	if (bitmapFileHeader.bfType != BITMAP_ID)
+	{
+		fclose(filePtr);
+		return NULL;
+	}
+
+	// read the bitmap information header
+	fread(bitmapInfoHeader, sizeof(BITMAPINFOHEADER), 1, filePtr);
+
+	// move file pointer to beginning of bitmap data
+	fseek(filePtr, bitmapFileHeader.bfOffBits, SEEK_SET);
+
+	// allocate enough memory for the bitmap image data
+	bitmapImage = (unsigned char*)malloc(bitmapInfoHeader->biSizeImage);
+
+	// verify memory allocation
+	if (!bitmapImage)
+	{
+		free(bitmapImage);
+		fclose(filePtr);
+		return NULL;
+	}
+
+	// read in the bitmap image data
+	fread(bitmapImage, 1, bitmapInfoHeader->biSizeImage, filePtr);
+
+	// make sure bitmap image data was read
+	if (bitmapImage == NULL)
+	{
+		fclose(filePtr);
+		return NULL;
+	}
+
+	// swap the R and B values to get RGB since the bitmap color format is in BGR
+	for (imageIdx = 0; imageIdx < bitmapInfoHeader->biSizeImage; imageIdx+=3)
+	{
+		tempRGB = bitmapImage[imageIdx];
+		bitmapImage[imageIdx] = bitmapImage[imageIdx + 2];
+		bitmapImage[imageIdx + 2] = tempRGB;
+	}
+
+	// close the file and return the bitmap image data
+	fclose(filePtr);
+	return bitmapImage;
+}
+
+
 float _angle = 60.0f;
 Terrain* _terrain;
+BITMAPINFOHEADER	landInfo;
+unsigned char*       landTexture;
+unsigned int		   land;
+
 
 void cleanup() {
 	delete _terrain;
@@ -221,6 +300,23 @@ void handleKeypress(unsigned char key, int x, int y) {
 	}
 }
 
+bool LoadTextures()
+{
+	// load the land texture data
+	landTexture = LoadBitmapFile("Terrain2.bmp", &landInfo);
+	if (!landTexture)
+		return false;
+
+	// generate the land texture as a mipmap
+	glGenTextures(1, &land);                  
+	glBindTexture(GL_TEXTURE_2D, land);       
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGB, landInfo.biHeight, landInfo.biWidth, GL_RGB, GL_UNSIGNED_BYTE, landTexture);
+
+	return true;
+}
+
 void initRendering() {
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_COLOR_MATERIAL);
@@ -228,6 +324,8 @@ void initRendering() {
 	glEnable(GL_LIGHT0);
 	glEnable(GL_NORMALIZE);
 	glShadeModel(GL_SMOOTH);
+	glEnable(GL_TEXTURE_2D);
+	LoadTextures();
 }
 
 void handleResize(int w, int h) {
@@ -244,7 +342,7 @@ void drawScene() {
 	glLoadIdentity();
 	glTranslatef(0.0f, 0.0f, -10.0f);
 	glRotatef(30.0f, 1.0f, 0.0f, 0.0f);
-	glRotatef(-_angle, 0.0f, 1.0f, 0.0f);
+	glRotatef(-1, 0.0f, 1.0f, 0.0f);
 	
 	GLfloat ambientColor[] = {0.4f, 0.4f, 0.4f, 1.0f};
 	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientColor);
@@ -254,22 +352,29 @@ void drawScene() {
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor0);
 	glLightfv(GL_LIGHT0, GL_POSITION, lightPos0);
 	
-	float scale = 5.0f / max(_terrain->width() - 1, _terrain->length() - 1);
+	float scale = 50.0f / max(_terrain->width() - 1, _terrain->length() - 1);
 	glScalef(scale, scale, scale);
 	glTranslatef(-(float)(_terrain->width() - 1) / 2,
 				 0.0f,
 				 -(float)(_terrain->length() - 1) / 2);
 	
-	glColor3f(0.3f, 0.9f, 0.0f);
+	//glColor3f(0.3f, 0.9f, 0.0f);
+	
+//	glBindTexture(GL_TEXTURE_2D,&land);
 	for(int z = 0; z < _terrain->length() - 1; z++) {
 		//Makes OpenGL draw a triangle at every three consecutive vertices
 		glBegin(GL_TRIANGLE_STRIP);
 		for(int x = 0; x < _terrain->width(); x++) {
-			Vec3f normal = _terrain->getNormal(x, z);
+			Vec3f normal = _terrain->getNormal(x, z);			
 			glNormal3f(normal[0], normal[1], normal[2]);
+			//glTexCoord2f(z/_terrain->width(),x/_terrain->length());
+			glTexCoord2f(z,x);
 			glVertex3f(x, _terrain->getHeight(x, z), z);
-			normal = _terrain->getNormal(x, z + 1);
+
+			normal = _terrain->getNormal(x, z + 1);			
 			glNormal3f(normal[0], normal[1], normal[2]);
+			//glTexCoord2f((z/_terrain->width())+1,(x/_terrain->length())+1);
+			glTexCoord2f(z+1,x+1);
 			glVertex3f(x, _terrain->getHeight(x, z + 1), z + 1);
 		}
 		glEnd();
@@ -288,15 +393,20 @@ void update(int value) {
 	glutTimerFunc(25, update, 0);
 }
 
+
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(400, 400);
 	
-	glutCreateWindow("Terrain - videotutorialsrock.com");
+
+	
+	
+	glutCreateWindow("Terrain");
 	initRendering();
 	
 	_terrain = loadTerrain("heightmap.bmp", 20);
+	
 	
 	glutDisplayFunc(drawScene);
 	glutKeyboardFunc(handleKeypress);
@@ -306,6 +416,7 @@ int main(int argc, char** argv) {
 	glutMainLoop();
 	return 0;
 }
+
 
 
 
